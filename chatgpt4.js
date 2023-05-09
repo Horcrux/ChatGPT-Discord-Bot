5eb31139837a700d1ca728dece6053b3ee3cc66f
Vincent <vincent.matthee97@gmail.com>
2023-05-09 19:32:02 +0200

    Added token count management to prevent errors related to token usage

--- chatgpt4.js
+++ chatgpt4.js
@@ -2,6 +2,8 @@ import { Client, Collection, Events, GatewayIntentBits } from "discord.js"
 import { Configuration, OpenAIApi } from "openai"
 import secrets from "./secrets.json" assert { type: "json" };
 
+import {encode} from "gpt-3-encoder"
+
 const bot_name = "machine"
 const token = secrets.discord_token; //discord
 const configuration = new Configuration({
@@ -10,6 +12,8 @@ const configuration = new Configuration({
 let temperature = 0.7;
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers] });
 const openai = new OpenAIApi(configuration);
+let totalTokens = 0;
+let currentTokens = 0;
 
 var history, mainChannel, testChannel, ai, txt
 var historyLen = 2000
@@ -44,10 +48,13 @@ var messages = [{ 'role': "user", 'content': "hello" }]
 
 var ai
 client.on('messageCreate', async (message) => {
+	console.log(`[${message.guild.name}][#${message.channel.name}][${message.author.tag}] ${message.content}`);
+
 	if (message.author.id.includes('some super special string here guys') && !message.content.toLowerCase().includes('npc, ')) {
 		return false
 	}
 	txt = message.content.replace(/[\\$'"]/g, "\\$&")
+	let tokens = encode(txt);
 
 	if (message.content.toLowerCase().includes(`${bot_name}:`) ||
 		message.content.toLowerCase().includes(`${bot_name},`) ||
@@ -57,6 +64,7 @@ client.on('messageCreate', async (message) => {
 		message.content.toLowerCase().includes(`${bot_name} `) ||
 		message.content.toLowerCase().includes(`${bot_name}!`)) {
 		message.channel.sendTyping()
+
 		if (message.author.tag.includes('real npc#5859')) { return false; } //bots name
 
 		if (message.content.toLowerCase().includes("gimme the temp")) {
@@ -71,7 +79,7 @@ client.on('messageCreate', async (message) => {
 		}
 
 		if (message.content.toLowerCase().includes('forget everything')) {
-			messages = [{ 'role': "user", 'content': "hi" }]
+			messages = [{ 'role': "user", 'content': "hello" }]
 			message.reply('i kinda feel funy ;o')
 			return false
 		}
@@ -80,10 +88,18 @@ client.on('messageCreate', async (message) => {
 			console.log('self')
 			return false
 		}
-		// txt = txt.replace(bot_name, "");
-
 
 		messages.push({ 'role': 'user', 'content': txt })
+		currentTokens = currentTokens + tokens.length + 9;
+		console.log(totalTokens);
+		console.log(currentTokens);
+		while ((currentTokens + totalTokens + 1500) > 4096) {
+			console.log("Token Threshold " + (currentTokens + totalTokens + 1500))
+			console.log("Forgor " + messages[0].content);
+			// message.reply("SYSTEM MESSAGE: we're running out of tokens, so im forgetting some stuff");
+			totalTokens = totalTokens - (encode(messages[0].content).length + 9);
+			messages.shift();
+		}
 
 		try {
 			ai = await openai.createChatCompletion({
@@ -94,17 +110,19 @@ client.on('messageCreate', async (message) => {
 				'max_tokens': 1500,
 				'user': message.author.id
 			});
-			console.log(ai)
+			totalTokens = ai.data.usage.total_tokens;
+			currentTokens = 0;
+			// console.log(ai)
 
-			if (ai.data.usage.total_tokens > 2500) {
-				message.reply("SYSTEM MESSAGE: we're running out of tokens, so im forgetting some stuff")
-				messages.shift();
+			console.log(ai.data.usage);
+		} catch (error) {
+			if (error.response.status == 429) {
+				message.reply("SYSTEM MESSAGE: Slow down, rate limit hit");
+			} else {
+				message.reply("SYSTEM MESSAGE: Unexpected error");
 			}
 
-
-		} catch (error) {
-			message.reply('error')
-			console.log(error)
+			// console.log(error)
 			console.log(error.response.status);
 			console.log(error.response.data);
 			console.log(error.message);
@@ -113,7 +131,7 @@ client.on('messageCreate', async (message) => {
 
 		if (ai.data.choices[0].message) {
 			let response = ai.data.choices[0].message
-			console.log("response ", response.content)
+			// console.log("response ", response.content)
 			var chunks = splitTextIntoChunks(response.content, 2000);
 			let i = 0
 			while (i < chunks.length) {
@@ -128,13 +146,10 @@ client.on('messageCreate', async (message) => {
 			}
 			//console.log(ai)
 			//console.log(ai.data.choices[0].message)
-
-
 		}
 	}
-	messages.push({ 'role': 'user', 'content': txt })
-
-	console.log(`[${message.guild.name}][#${message.channel.name}][${message.author.tag}] ${message.content}`);
+	messages.push({ 'role': 'user', 'content': txt });
+	currentTokens = currentTokens + tokens.length;
 })
 
 client.login(token);
